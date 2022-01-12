e5b52be4012043ba1fb7561ed15e4291ad084cf9
saint-lag <maiasena.gabriel@gmail.com>
2022-01-12 15:09:12 -0300

    Ajeitando estética

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-const email = document.getElementById('email');
-const senha = document.getElementById('password');
-const buttonEnviar = document.getElementById('submit-btn');
-const agreement = document.getElementById('agreement');
+const email = document.getElementById("email");
+const senha = document.getElementById("password");
+const buttonEnviar = document.getElementById("submit-btn");
+const agreement = document.getElementById("agreement");
 
-document.querySelector('#login-button').addEventListener('click', (event) => {
+document.querySelector("#login-button").addEventListener("click", (event) => {
   event.preventDefault();
-  if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-    alert('Olá, Tryber!');
+  if (email.value === "tryber@teste.com" && senha.value === "123456") {
+    alert("Olá, Tryber!");
   } else {
-    alert('Email ou senha inválidos.');
+    alert("Email ou senha inválidos.");
   }
 });
 
 function habilitaButton() {
-  console.log(document.getElementById('agreement').value);
-  if (document.getElementById('agreement').value === '') {
+  console.log(document.getElementById("agreement").value);
+  if (document.getElementById("agreement").value === "") {
     buttonEnviar.disabled = true;
   } else {
     buttonEnviar.disabled = false;
   }
 }
 
-agreement.addEventListener('change', habilitaButton);
+agreement.addEventListener("change", habilitaButton);
