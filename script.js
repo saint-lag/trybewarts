58cfce280b7c89979ae6dffb52dabe76488e6a8b
saint-lag <maiasena.gabriel@gmail.com>
2022-01-11 21:15:59 -0300

    Juntando novas implementações no forms

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
-var email = document.getElementById('email');
-var senha = document.getElementById('password');
-const buttonEnviar = document.getElementById('submit-btn');
-const agreement = document.getElementById('agreement');
+var email = document.getElementById("email");
+var senha = document.getElementById("password");
+const buttonEnviar = document.getElementById("submit-btn");
+const agreement = document.getElementById("agreement");
 
 // Problema na funcionalidade do formulário (alerta)
 
-document.querySelector('#login-button').addEventListener('click', () => {
-    event.preventDefault();
-    if (email.value === 'tryber@teste.com' && senha.value === '123456') {
-      alert('Olá, Tryber!');
-    } else if (email === '' || senha === '') {
+document.querySelector("#login-button").addEventListener("click", () => {
+  event.preventDefault();
+  if (email.value === "tryber@teste.com" && senha.value === "123456") {
+    alert("Olá, Tryber!");
+  } else if (email === "" || senha === "") {
   } else {
-      alert('Email ou senha inválidos.');
-    }
-})
+    alert("Email ou senha inválidos.");
+  }
+});
 
 function habilitaButton() {
-  if (document.getElementById('agreement').value === '') {
+  if (document.getElementById("agreement").value === "") {
     buttonEnviar.disabled = true;
   } else {
     buttonEnviar.disabled = false;
-    console.log('teste');
+    console.log("teste");
   }
 }
 
-agreement.addEventListener('change', habilitaButton); 
+agreement.addEventListener("change", habilitaButton);
