dbc2d6b0c2a6ff6525a4d2b583a562050243b663
saint-lag <maiasena.gabriel@gmail.com>
2022-01-16 17:50:01 -0300

    Stylint: script.js

--- script.js
+++ script.js
@@ -41,27 +41,35 @@ function addChildP(chave, valor) {
   evaluationForm.appendChild(p);
 }
 
+function informationGetter() {
+  const information = [
+    `${document.getElementById('input-name').value} ${
+      document.getElementById('input-lastname').value
+    }`,
+    document.getElementById('input-email').value,
+    document.getElementById('house').options[
+      document.getElementById('house').selectedIndex
+    ].value,
+    document.querySelector('input[name="family"]:checked').value,
+    valueCheckbox(document.getElementsByName('content')),
+    document.querySelector('input[name="rate"]:checked').value,
+    document.getElementById('textarea').value,
+  ];
+
+  return information;
+}
+
 buttonEnviar.addEventListener('click', (event) => {
+  const information = informationGetter();
   event.preventDefault();
-  const nameValue = document.getElementById('input-name').value;
-  const nameComplete = `${nameValue} ${
-    document.getElementById('input-lastname').value
-  }`;
-  const emailValue = document.getElementById('input-email').value;
-  const select = document.getElementById('house');
-  const house = select.options[select.selectedIndex].value;
-  const family = document.querySelector('input[name="family"]:checked').value;
-  const rate = document.querySelector('input[name="rate"]:checked').value;
-  const comentary = document.getElementById('textarea').value;
-  const contentCheckbox = valueCheckbox(document.getElementsByName('content'));
   evaluationForm.innerHTML = '';
-  addChildP('Nome', nameComplete);
-  addChildP('Email', emailValue);
-  addChildP('Casa', house);
-  addChildP('Família', family);
-  addChildP('Matérias', contentCheckbox);
-  addChildP('Avaliação', rate);
-  addChildP('Observações', comentary);
+  addChildP('Nome', information[0]);
+  addChildP('Email', information[1]);
+  addChildP('Casa', information[2]);
+  addChildP('Família', information[3]);
+  addChildP('Matérias', information[4]);
+  addChildP('Avaliação', information[5]);
+  addChildP('Observações', information[6]);
 });
 
 agreement.addEventListener('change', habilitaButton);
